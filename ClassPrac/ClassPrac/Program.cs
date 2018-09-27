f69ef01fce22baa3f88a860231387ccebf5586f6
Luke Fox <30011364@student.toiohomai.ac.nz>
2018-09-27 12:01:06 +1200

    message

---
+++ ClassPrac/ClassPrac/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassPrac
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+    class Pizza
+    {
+        public bool Cheese { get; set; }
+        public  string Crust { get; set; }
+        public string Sause { get; set; }
+        public decimal Price { get; set; }
+        public decimal Discount { get; set; }
+
+        public Pizza(bool _cheese, string _crust, string _sause, decimal _price, decimal _discount)
+        {
+            Cheese = _cheese;
+            Crust = _crust;
+            Sause = _sause;
+            Price = _price;
+            Discount = _discount;
+        }
+        public bool 
+      
+    }
+
+}  
